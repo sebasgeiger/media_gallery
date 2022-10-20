d827e07fc0be1ba8b76377d09c2678fc888a9829
Sebastian Geiger <geigersr@clarkson.edu>
2022-10-20 15:49:34 -0400

    setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# eg_media_starter
+ project starter for media gallery
---
+++ js/page.js
@@ -0,0 +1,4 @@
+// a test function that runs when the button is clicked
+function sampleFunction() {
+  alert("You clicked the button");
+}
