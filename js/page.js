636d638e76868dc333edaf85364eb79692e6536d
Sebastian Geiger <geigersr@clarkson.edu>
2022-10-28 14:55:31 -0400

    finishing touches

--- js/page.js
+++ js/page.js
@@ -14,7 +14,6 @@ function navToggle() {
     trigram.classList = ('nav-link');
   }
 }
-
 // Slideshow Functions
 var slideIndex = 1; 
 
@@ -27,7 +26,6 @@ function plusSlides(n) {
 function currentSlide(n) { 
   showSlides(slideIndex = n); 
 }
-
 function showSlides(n) { 
   var i; 
   var slides = document.getElementsByClassName("slide"); 
@@ -43,4 +41,15 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   indicator[slideIndex-1].className += " active"; 
 }
+// Image modal functions - Requires jQuery!!!
+$("button").on("click", function() { // when item with class of button is clicked, fire function
+  var modal = $(this).data("modal"); // sets modal var equal to data attribute
+  $(modal).show(); // opens up modal (much code hidden here by jQuery)
+});
+$(".modal").on("click", function(e) { // sets up click function
+  var className = e.target.className; // var className set to event target
+  if(className === "modal" || className === "close"){ // if conditions are met...
+    $(this).closest(".modal").hide(); // then hide the modal
+  } // closes if-statement
+}); // closes function
 
